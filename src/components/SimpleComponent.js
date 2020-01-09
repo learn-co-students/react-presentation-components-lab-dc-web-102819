43143e62ea0df8b17a35a0430f264eb8209c0a5d
mattbechtel1 <bechma02@gmail.com>
2020-01-09 17:29:00 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,19 @@
 // Code SimpleComponent Here
+import React from 'react'
+
+export default class SimpleComponent extends React.Component {
+    constructor() {
+        super()
+        this.state = {mood: 'happy'}
+    }
+
+    handleClick = () => {
+        const inverse = { 'happy' : 'sad', 'sad' : 'happy'}
+        this.setState({mood: inverse[this.state.mood]})
+    }
+
+    render() {
+        return <div onClick={this.handleClick}>{this.state.mood}</div>
+    }
+
+}
